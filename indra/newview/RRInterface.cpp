3430c018fcfd431816d3bdb6227d328182288901
Marine Kelley <none@none>
2019-02-20 10:43:48 +0100

    Clean up after rendering the vision spheres (not sure this is needed, but it doesn't hurt)

--- indra/newview/RRInterface.cpp
+++ indra/newview/RRInterface.cpp
@@ -5344,10 +5344,11 @@ void RRInterface::drawRenderLimit ()
 	}
 
 	gGL.flush();
+	gGL.setColorMask(true, false);
 
-	//if (LLGLSLShader::sNoFixedFunction) {
-	//	gUIProgram.unbind();
-	//}
+	if (LLGLSLShader::sNoFixedFunction) {
+		gUIProgram.unbind();
+	}
 
 	sRenderLimitRenderedThisFrame = TRUE;
 }
