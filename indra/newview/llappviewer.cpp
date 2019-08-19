5f5cffdeea53ce0dfe1c65fbe6bcd234008776c9
Chorazin Allen <chorazinallen@gmail.com>
2019-08-19 16:29:27 +0100

    Configure the first-run page to show topic posts in the blog

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -3221,9 +3221,10 @@ LLSD LLAppViewer::getViewerInfo() const
 	// return a URL to the release notes for this viewer, such as:
 	// https://releasenotes.secondlife.com/viewer/2.1.0.123456.html
 	std::string url = LLTrans::getString("RELEASE_NOTES_BASE_URL");
-	if (! LLStringUtil::endsWith(url, "/"))
-		url += "/";
-	url += LLURI::escape(LLVersionInfo::getVersion()) + ".html";
+	// With RLV we just show the appropriate blog thread, so appending the version isn't required here
+	//if (! LLStringUtil::endsWith(url, "/"))
+	//	url += "/";
+	//url += LLURI::escape(LLVersionInfo::getVersion()) + ".html";
 
 	info["VIEWER_RELEASE_NOTES_URL"] = url;
 
