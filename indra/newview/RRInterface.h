378fe34ccc1db506ad2fba5a4733738c4ef8eaa3
Chorazin Allen <chorazinallen@gmail.com>
2019-10-30 00:17:20 +0000

    Update RLV version and parent LL version numbers

--- indra/newview/RRInterface.h
+++ indra/newview/RRInterface.h
@@ -30,9 +30,9 @@
 
 #define RR_VIEWER_NAME "RestrainedLife"
 #define RR_VIEWER_NAME_NEW "RestrainedLove"
-#define RR_VERSION_NUM "2092604"
-#define RR_VERSION "2.09.26.04"
-#define RR_SLV_VERSION "6.3.0"
+#define RR_VERSION_NUM "2092605"
+#define RR_VERSION "2.09.26.05"
+#define RR_SLV_VERSION "6.3.3"
 
 #define RR_PREFIX "@"
 #define RR_SHARED_FOLDER "#RLV"
