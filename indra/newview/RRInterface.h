828c5b9eb301204e98c7b8d5ca6003c1f2a2ba06
Chorazin Allen <chorazinallen@gmail.com>
2020-02-21 10:46:43 +0000

    Nudge the base LL version to 6.3.7

--- indra/newview/RRInterface.h
+++ indra/newview/RRInterface.h
@@ -32,7 +32,7 @@
 #define RR_VIEWER_NAME_NEW "RestrainedLove"
 #define RR_VERSION_NUM "2092605"
 #define RR_VERSION "2.09.26.05"
-#define RR_SLV_VERSION "6.3.6"
+#define RR_SLV_VERSION "6.3.7"
 
 #define RR_PREFIX "@"
 #define RR_SHARED_FOLDER "#RLV"
