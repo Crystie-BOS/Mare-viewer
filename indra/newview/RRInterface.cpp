97c92edc693fa3a77961b96083519b7895b847ba
Chorazin Allen <chorazinallen@gmail.com>
2020-04-28 15:14:37 +0100

    Add @shownearby to clear the nearby list and turn on anonymising of names in the People and Minimap green dot tooltips. Implementation is disabled - to enable it temporarily comment out the #define in RRInterface or permanently by removing all references to the #define. Also added a previously missed initialiser for the viewscript global.

--- indra/newview/RRInterface.cpp
+++ indra/newview/RRInterface.cpp
@@ -102,6 +102,8 @@
 #include "lluicolortable.h"
 //ca
 
+#define DISABLE_SHOWNEARBY
+
 // Global and static variables initialization.
 BOOL gRRenabled = TRUE;
 BOOL RRInterface::sRRNoSetEnv = FALSE;
@@ -425,6 +427,9 @@ void refreshCachedVariable (std::string var)
 	else if (var == "showloc")				gAgent.mRRInterface.mContainsShowloc = contained;
 	//else if (var == "shownames")			gAgent.mRRInterface.mContainsShownames = contained;
 	else if (var == "shownametags")			gAgent.mRRInterface.mContainsShownametags = contained;
+#ifndef DISABLE_SHOWNEARBY
+	else if (var == "shownearby")			gAgent.mRRInterface.mContainsShowNearby = contained;
+#endif
 	else if (var == "setenv")				gAgent.mRRInterface.mContainsSetenv = contained;
 	else if (var == "setdebug")				gAgent.mRRInterface.mContainsSetdebug = contained;
 	else if (var == "fly")					gAgent.mRRInterface.mContainsFly = contained;
@@ -485,8 +490,12 @@ void refreshCachedVariable (std::string var)
 		}
 	}
 
+#ifndef DISABLE_SHOWNEARBY
 	else if (var == "shownames" || var == "shownames_sec" || var == "shownametags") {
-		if (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags) {
+#else
+	else if (var == "shownames" || var == "shownames_sec" || var == "shownametags" || var == "shownearby") {
+#endif
+		if (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags || gAgent.mRRInterface.mContainsShowNearby) {
 //			LLSideTray::getInstance()->childSetVisible("nearby_panel", false);
 //			LLSideTray::getInstance()->childSetVisible("recent_panel", false);
 			LLPanel* panel = LLFloaterSidePanelContainer::getPanel("people", "panel_people");
@@ -714,6 +723,8 @@ RRInterface::RRInterface():
 	, mContainsShowloc(FALSE)
 	, mContainsShownames(FALSE)
 	, mContainsShownametags(FALSE)
+	, mContainsShowNearby(FALSE)
+	, mContainsViewScript(FALSE)
 	, mContainsSetenv(FALSE)
 	, mContainsSetdebug(FALSE)
 	, mContainsFly(FALSE)
--- indra/newview/llnetmap.cpp
+++ indra/newview/llnetmap.cpp
@@ -359,7 +359,7 @@ void LLNetMap::draw()
 //MK
 				// Don't show as friend under @shownames, since it can give away an
 				// information about the avatars who are around
-				if (gRRenabled && (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags))
+				if (gRRenabled && (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags || gAgent.mRRInterface.mContainsShowNearby))
 				{
 					show_as_friend = false;
 				}
@@ -677,7 +677,7 @@ BOOL LLNetMap::handleToolTipAgent(const LLUUID& avatar_id)
 		p.message(av_name.getCompleteName());
 //MK
 		std::string label = av_name.getCompleteName();
-		if (gRRenabled && (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags))
+		if (gRRenabled && (gAgent.mRRInterface.mContainsShownames || gAgent.mRRInterface.mContainsShownametags || gAgent.mRRInterface.mContainsShowNearby))
 		{
 			label = gAgent.mRRInterface.getDummyName(av_name.mUsername);
 		}
