dcf61b093ed7a528ee1fbfbc7c006bf620f424d9
MarineKelley <marinekelley@gmail.com>
2020-10-22 17:51:27 +0200

    Fix : We couldn't open the Build window with the Build button or Ctrl+B, but we could with Ctrl+3.

--- indra/newview/lltoolmgr.cpp
+++ indra/newview/lltoolmgr.cpp
@@ -275,7 +275,7 @@ void LLToolMgr::toggleBuildMode(const LLSD& sdname)
 
 
 //MK
-	if (gRRenabled && (gAgent.mRRInterface.mContainsEdit || gAgent.mRRInterface.mContainsRez || gAgent.mRRInterface.containsSubstr ("editobj:")))
+	if (gRRenabled && (gAgent.mRRInterface.mContainsEdit || gAgent.mRRInterface.mContainsRez))
 	{
 		return;
 	}
