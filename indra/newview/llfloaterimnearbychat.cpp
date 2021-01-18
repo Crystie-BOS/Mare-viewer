72843a3e2b1f6862787ff9bb2103e2d08d5e345e
Chorazin Allen <chorazinallen@gmail.com>
2021-01-18 14:57:28 +0000

    KKA-789 Redirected chat optionally triggers typing notification - implementation (UI will be in subsequent viewer-specific commits)

--- indra/newview/llfloaterimnearbychat.cpp
+++ indra/newview/llfloaterimnearbychat.cpp
@@ -474,7 +474,7 @@ void LLFloaterIMNearbyChat::onChatBoxKeystroke()
 //mk
 	{
 //MK
-		if (!gRRenabled || !gAgent.mRRInterface.containsSubstr ("redirchat:"))
+		if (!gRRenabled || !gAgent.mRRInterface.containsSubstr ("redirchat:") || gSavedSettings.getBOOL("RestrainedLoveShowRedirectChatTyping"))
 //mk
 		gAgent.startTyping();
 	}
@@ -815,7 +815,7 @@ void LLFloaterIMNearbyChat::sendChatFromViewer(const LLWString &wtext, EChatType
 			type = CHAT_TYPE_WHISPER;
 		}
 		
-		if (gAgent.mRRInterface.containsSubstr ("redirchat:"))
+		if (gAgent.mRRInterface.containsSubstr ("redirchat:") && !gSavedSettings.getBOOL("RestrainedLoveShowRedirectChatTyping"))
 		{
 			animate = false;
 		}
