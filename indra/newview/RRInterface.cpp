e9a64e63e66cbf5eb73912ec7d97db441a200f1f
Chorazin Allen <chorazinallen@gmail.com>
2023-04-02 16:07:19 +0100

    Mac build fix

--- indra/newview/RRInterface.cpp
+++ indra/newview/RRInterface.cpp
@@ -2721,7 +2721,7 @@ std::string RRInterface::getStatus (LLUUID object_uuid, std::string rule)
 	else {
 		it = mSpecialObjectBehaviours.find (object_uuid.asString());
 	}
-	bool is_first=true;
+//unused	bool is_first=true;
 	while (it != mSpecialObjectBehaviours.end() &&
 			(object_uuid.isNull() || it != mSpecialObjectBehaviours.upper_bound(object_uuid.asString()))
 	)
@@ -2730,7 +2730,7 @@ std::string RRInterface::getStatus (LLUUID object_uuid, std::string rule)
 			//if (!is_first) 
 			res+=separator;
 			res+=it->second;
-			is_first=false;
+//unused			is_first=false;
 		}
 		it++;
 	}
