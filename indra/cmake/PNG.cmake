f49ba11c159140716470edc2ccfea989f0c8c194
Erik Kundiman <erik@megapahit.org>
2023-07-15 19:46:10 +0800

    KKA-1026 Fix Linux crash when opening file picker
    Basically brought back some part that didn't make it to the modernised
    CMake settings (commit 8c66f9). Funny how on some other setup I happen
    to have, the file picker opens just fine without this brought back. And
    I still don't know exactly how PNG relates to file picker. Maybe
    somewhere in Kokua (and MK RLV too) there is some code that's making a
    specific call to libpng when trying to pick a file.

--- indra/cmake/PNG.cmake
+++ indra/cmake/PNG.cmake
@@ -12,3 +12,18 @@ else()
   target_link_libraries(ll::libpng INTERFACE png16 )
 endif()
 target_include_directories( ll::libpng SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include/libpng16)
+if (LINUX)
+  #
+  # When we have updated static libraries in competition with older
+  # shared libraries and we want the former to win, we need to do some
+  # extra work.  The *_PRELOAD_ARCHIVES settings are invoked early
+  # and will pull in the entire archive to the binary giving it 
+  # priority in symbol resolution.  Beware of cmake moving the
+  # achive load itself to another place on the link command line.  If
+  # that happens, you can try something like -Wl,-lpng16 here to hide
+  # the archive.  Also be aware that the linker will not tolerate a
+  # second whole-archive load of the archive.  See viewer's
+  # CMakeLists.txt for more information.
+  #
+  set(PNG_PRELOAD_ARCHIVES -Wl,--whole-archive png16 -Wl,--no-whole-archive)
+endif ()
