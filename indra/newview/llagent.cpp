3580b6d00b302cee1ca4eb29688c3fc4526b9f65
Chorazin Allen <chorazinallen@gmail.com>
2023-09-05 04:14:06 +0100

    Simpler version of fix for GCC/Linux

--- indra/newview/llagent.cpp
+++ indra/newview/llagent.cpp
@@ -403,8 +403,11 @@ LLAgent::LLAgent() :
 	mHttpPolicy(LLCore::HttpRequest::DEFAULT_POLICY_ID),
 	mTeleportState(TELEPORT_NONE),
 	mRegionp(NULL),
+#if LL_LINUX
+    mInterestListMode("default"), // since GCC can't figure this out
+#else
     mInterestListMode(LLViewerRegion::IL_MODE_DEFAULT),
-
+#endif
 	mAgentOriginGlobal(),
 	mPositionGlobal(),
     mLastTestGlobal(),
