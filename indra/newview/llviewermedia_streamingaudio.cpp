mobserveur/viewer

Custom channel stream notif sent as if from viewer
As if it was the user typing "/157157 Now playing blabla".

indra/newview/llviewermedia_streamingaudio.cpp
@@ -108,6 +108,8 @@ void LLStreamingAudio_MediaPlugins::update()
         mMediaPlugin->idle();
 }
 
+extern void send_chat_from_viewer(const std::string& utf8_out_text, EChatType type, S32 channel);
+
 int LLStreamingAudio_MediaPlugins::isPlaying()
 {
     if (!mMediaPlugin)
@@ -120,10 +122,18 @@ int LLStreamingAudio_MediaPlugins::isPlaying()
     {
         mTitle = mMediaPlugin->getMediaTitle();
         mNowPlaying = nowPlaying;
-        LLChat chat{llformat("Now playing %s.", nowPlaying.c_str())};
-        chat.mFromName = mTitle;
-        chat.mSourceType = CHAT_SOURCE_SYSTEM;
-        LLNotificationsUI::LLNotificationManager::instance().onChat(chat, LLSD{});
+        auto text = llformat("Now playing %s.", nowPlaying.c_str());
+        if (gSavedSettings.getBOOL("StreamNotificationChannelEnabled"))
+        {
+            send_chat_from_viewer(text, CHAT_TYPE_NORMAL, gSavedSettings.getS32("StreamNotificationChannel"));
+        }
+        else
+        {
+            LLChat chat{text};
+            chat.mFromName = mTitle;
+            chat.mSourceType = CHAT_SOURCE_SYSTEM;
+            LLNotificationsUI::LLNotificationManager::instance().onChat(chat, LLSD{});
+        }
     }
 
     switch (status)

Crystie-BOS/Mare-viewer

indra/newview/llviewermedia_streamingaudio.cpp
@@ -159,6 +159,8 @@ void LLStreamingAudio_MediaPlugins::update()
         mMediaPlugin->idle();
 }
 
+extern void send_chat_from_viewer(const std::string& utf8_out_text, EChatType type, S32 channel);
+
 int LLStreamingAudio_MediaPlugins::isPlaying()
 {
     if (!mMediaPlugin)
@@ -171,10 +173,18 @@ int LLStreamingAudio_MediaPlugins::isPlaying()
     {
         mTitle = mMediaPlugin->getMediaTitle();
         mNowPlaying = nowPlaying;
-        LLChat chat{llformat("Now playing %s.", nowPlaying.c_str())};
-        chat.mFromName = mTitle;
-        chat.mSourceType = CHAT_SOURCE_SYSTEM;
-        LLNotificationsUI::LLNotificationManager::instance().onChat(chat, LLSD{});
+        auto text = llformat("Now playing %s.", nowPlaying.c_str());
+        if (gSavedSettings.getBOOL("StreamMetadataAnnounceToChat"))
+        {
+            send_chat_from_viewer(text, CHAT_TYPE_NORMAL, gSavedSettings.getS32("StreamMetadataAnnounceChannel"));
+        }
+        else
+        {
+            LLChat chat{text};
+            chat.mFromName = mTitle;
+            chat.mSourceType = CHAT_SOURCE_SYSTEM;
+            LLNotificationsUI::LLNotificationManager::instance().onChat(chat, LLSD{});
+        }
     }
 
     switch (status)