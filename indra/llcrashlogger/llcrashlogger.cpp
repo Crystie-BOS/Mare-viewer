56a09e6aa32efaf259be25d13cd401a4675bc677
Chorazin Allen <chorazinallen@gmail.com>
2025-10-09 11:39:01 +0100

    Revert the crashlogger change that slipped into the minimap PR

--- indra/llcrashlogger/llcrashlogger.cpp
+++ indra/llcrashlogger/llcrashlogger.cpp
@@ -685,7 +685,7 @@ bool LLCrashLogger::init()
     std::string log_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS, "crashreport.log");
 
 #if LL_WINDOWS
-    LLFile::remove(old_log_file, ENOENT);
+    LLAPRFile::remove(old_log_file);
 #endif
 
     LLFile::rename(log_file.c_str(), old_log_file.c_str());
