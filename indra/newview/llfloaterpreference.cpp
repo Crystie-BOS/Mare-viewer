6c54124e9a20588c9fcebdb4dd6d33ddb069f0d7
Chorazin Allen <chorazinallen@gmail.com>
2026-04-16 23:29:00 +0100

    Rename default font as Inter where it appears in the font selector (Prefs/Kokua/Tweaks) and add Deja VU as another font option so that people can revert if they wish

--- indra/newview/llfloaterpreference.cpp
+++ indra/newview/llfloaterpreference.cpp
@@ -4958,7 +4958,7 @@ void LLFloaterPreference::loadFontPresetsFromDir(const std::string& dir, LLCombo
         //hack to deal with "fonts.xml"
         if (file == "fonts.xml")
         {
-            font_selection_combo->add("Deja Vu", file);
+            font_selection_combo->add("Inter", file);
         }
         //hack to get "fonts_[name].xml" to "Name"
         else
