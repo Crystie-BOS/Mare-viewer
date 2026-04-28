a741594ead1768b28420897f346f46532b4c8741
Kentron Katana <kentron@kokuaviewer.org>
2026-04-28 21:59:31 +0200

    Add "Create folder from selected" to Places floater, folder and landmark, gear menus (Landmarks and Favorites tabs).

--- indra/newview/llpanellandmarks.cpp
+++ indra/newview/llpanellandmarks.cpp
@@ -859,6 +859,11 @@ bool LLLandmarksPanel::isActionEnabled(const LLSD& userdata) const
         }
         return false;
     }
+    else if ("new_folder_from_selected" == command_name)
+    {
+        if (!root_folder_view) return false;
+        return !root_folder_view->getSelectionList().empty();
+    }
     else
     {
         LL_WARNS() << "Unprocessed command has come: " << command_name << LL_ENDL;
